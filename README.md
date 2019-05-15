65a268c890101add48f0d25940d9bf77b34c33d8
Tech-A06 <tech-a06@Tech-A06s-MacBook-Air.local>
2019-05-15 18:30:30 +0100

    Finihsed and got it working

---
+++ README.md
@@ -0,0 +1,15 @@
+# Sparta-Nginx-Reverse-Proxy
+
+## The task
+
+To use an Nginx reverse proxy to make the node port the root for our IP address
+
+## To Use
+
+Clone the repo down and run vagrant up. Then type
+
+`vagrant ssh app`
+`cd /vagrant/app`
+`pm2 start app.js`
+
+Fianlly, go to [this page](http://development.local) to see the app and [this page](http://development.local/posts) to see the database.
---
+++ environment/db/provision.sh
@@ -0,0 +1,14 @@
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+# sudo apt-get install mongodb-org=3.2.18 -y
+sudo apt-get install -y mongodb-org=3.2.20 mongodb-org-server=3.2.20 mongodb-org-shell=3.2.20 mongodb-org-mongos=3.2.20 mongodb-org-tools=3.2.20
+
+sudo rm /etc/mongod.conf #use this line but for nginx this
+sudo ln -s /home/ubuntu/environment/mongod.conf /etc/mongod.conf #use this to make a symbolic link -- links files so they can be mutually edited
+
+sudo systemctl restart mongod
+sudo systemctl enable mongod
